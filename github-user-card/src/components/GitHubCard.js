bbff3df266135e546911e41470c691dd92674f3c
AceIsHuman <menoman1995@yahoo.com>
2019-09-08 19:18:22 -0500

    display data

--- github-user-card/src/components/GitHubCard.js
+++ github-user-card/src/components/GitHubCard.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Card, Image } from 'semantic-ui-react';
 
 class GitHubCard extends React.Component {
   render() {
     return (
-      <h1>GitHubCard</h1>
+      <Card>
+        <Image src={this.props.githubUserData.avatar_url} />
+        <Card.Content>
+          <Card.Header>{this.props.githubUserData.login}</Card.Header>
+          <Card.Meta>{this.props.githubUserData.name}</Card.Meta>
+        </Card.Content>
+        <Card.Content extra>
+          <div>
+            <a href={this.props.githubUserData.html_url} target="_blank">View on GitHub</a>
+          </div>
+        </Card.Content>
+      </Card>
     )
   } 
 }

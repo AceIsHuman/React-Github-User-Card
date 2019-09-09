ce0fb898fcca49aee96ff687c8e239818d7d1ade
AceIsHuman <menoman1995@yahoo.com>
2019-09-08 19:33:30 -0500

    use componentDidUpdate to fetch followers data and pass using state

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -2,23 +2,33 @@ import React from 'react';
 import './App.css';
 
 import GitHubCard from './components/GitHubCard';
+import FollowersList from './components/FollowersList';
 
 class App extends React.Component {
   state = {
-    githubUserData: {}
+    githubUserData: {},
+    followers_url: '',
+    followers: []
   }
 
   componentDidMount() {
-    fetch('https://api.github.com/users/aceishuman')
+    fetch('https://api.github.com/users/ajb85')
       .then(res => res.json())
-      .then(res => this.setState({ githubUserData: res }))
+      .then(res => this.setState({ ...this.state, githubUserData: res, followers_url: res.followers_url}))
       .catch(err => console.error(err.response))
+  }
+
+  componentDidUpdate() {
+    fetch(this.state.githubUserData.followers_url)
+      .then(res => res.json())
+      .then(res => this.setState({...this.state, followers: res }))
   }
 
   render() {
     return (
       <div className="App">
         <GitHubCard githubUserData={this.state.githubUserData} />
+        <FollowersList followers={this.state.followers} />
       </div>
     );
   }

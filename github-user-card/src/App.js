5570882856500407e599f835d7d3a43836a09d08
AceIsHuman <menoman1995@yahoo.com>
2019-09-08 19:02:00 -0500

    fetch gitHubUserData and assign to state

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -4,6 +4,17 @@ import './App.css';
 import GitHubCard from './components/GitHubCard';
 
 class App extends React.Component {
+  state = {
+    githubUserData: {}
+  }
+
+  componentDidMount() {
+    fetch('https://api.github.com/users/aceishuman')
+      .then(res => res.json())
+      .then(res => this.setState({ githubUserData: res }))
+      .catch(err => console.error(err.response))
+  }
+
   render() {
     return (
       <div className="App">
